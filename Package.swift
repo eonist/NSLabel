2b5a3f83ef8a434be707360ca74f4a0dc82dd8f1
eonist <30n1st@gmail.com>
2020-08-21 00:45:49 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NSLabel",
+    platforms: [.macOS(.v10_13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
